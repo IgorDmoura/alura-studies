50a768c75ecf21657ebbd53667defcfbf7107113
Igor Ricardo de Moura <igor.moura@compasso.com.br>
2022-08-20 15:34:28 -0300

    Adição de novos componentes e uso de css modules

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,34 +1,36 @@
 import React from "react";
 import Botao from '../Botao';
+import style from './Formulario.module.scss'
 
 class Formulario extends React.Component{
     render(){
         return(
-            <form>
-                <div>
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
                     <input type="text"
-                     name="tarefa" 
-                     id="tarefa" 
-                     placeholder="Oque você quer estudar" 
+                     name="tarefa"
+                     id="tarefa"
+                     placeholder="Oque você quer estudar"
                      required />
                 </div>
+                <div className={style.inputContainer}>
                     <label htmlFor="tempo">
                         Tempo
                     </label>
                     <input type="time"
-                     step="1" 
-                     name="tempo" 
+                     step="1"
+                     name="tempo"
                      id="tempo"
-                     min="00:00:00" 
-                     max="01:30:00" 
+                     min="00:00:00"
+                     max="01:30:00"
                      required />
-                <div>
-
                 </div>
-                <Botao />
+                <Botao>
+                    Adicionar
+                </Botao>
             </form>
         )
     }
